d60ce5eb5ba0eb3f6c92320ff56eb84356e29518
prince_77396 <singhsahab130@gmail.com>
2023-03-27 10:32:17 +0530

    day4 removeduplicates

---
+++ Prince singh/day1/day2/day4/removeduplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int pen = 0;
+        for(int i = 1; i < nums.size(); ++i){
+            if(nums[i] != nums[pen]){
+                ++pen;
+                nums[pen] = nums[i];
+            }
+        }
+
+        return pen+1;
+    }
+};
