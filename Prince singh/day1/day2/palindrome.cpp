53ddd0e2260142e8ec843f8c8ef22297c322b0ce
prince_77396 <singhsahab130@gmail.com>
2023-03-27 10:25:55 +0530

    day3: add palindrome

---
+++ Prince singh/day1/day2/palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x >= 0) {
+            uint n = x, rev = 0;
+            while (n > 0) {
+                rev = (rev * 10) + (n % 10);
+                n = n / 10;
+            }
+            return rev == x;
+        }
+        return false;
+    }
+};
